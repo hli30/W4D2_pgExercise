efa00531f319ea3dd61738b2791b47351ef19ae3
hli30 <harrylll93@gmail.com>
2018-03-06 21:57:02 +0000

    initial commit

---
+++ lookup_people.js
@@ -0,0 +1,35 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+let input = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE last_name=$1;", [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    outputMsg(result);
+    client.end();
+  });
+});
+
+function outputMsg (result) {
+  console.log("Searching ...");
+  const count = result.rows.length;
+  result.rows.forEach((row) => {
+      let msg = `Found ${count} person(s) by the name '${row.last_name}': \n- ${row.id}, ${row.first_name} ${row.last_name}, born '${row.birthdate}'`;
+      console.log(msg);
+  });
+}
