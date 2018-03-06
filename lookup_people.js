3aec5a680773d6bb063110f230445c09a469ab54
hli30 <harrylll93@gmail.com>
2018-03-06 22:10:08 +0000

    Added knex and changed output string to take in input

--- lookup_people.js
+++ lookup_people.js
@@ -16,7 +16,7 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famous_people WHERE last_name=$1;", [input], (err, result) => {
+  client.query("SELECT * FROM famous_people WHERE last_name=$1 OR first_name=$1;", [input], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
@@ -29,7 +29,7 @@ function outputMsg (result) {
   console.log("Searching ...");
   const count = result.rows.length;
   result.rows.forEach((row) => {
-      let msg = `Found ${count} person(s) by the name '${row.last_name}': \n- ${row.id}, ${row.first_name} ${row.last_name}, born '${row.birthdate}'`;
+      let msg = `Found ${count} person(s) by the name '${input}': \n- ${row.id}, ${row.first_name} ${row.last_name}, born '${row.birthdate}'`;
       console.log(msg);
   });
 }
